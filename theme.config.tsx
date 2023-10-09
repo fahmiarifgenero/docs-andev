0677a665faf5ad001724bf0dc14a0d6bb38122bc
Fahmi Arif Genero <fahmi.arif.genero@gmail.com>
2023-10-09 09:52:07 +0700

    🔧 config nextra docs theme

---
+++ theme.config.tsx
@@ -0,0 +1,10 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const themeConfig: DocsThemeConfig = {
+  logo: <span>My Nextra Documentation</span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+};
+
+export default themeConfig;
